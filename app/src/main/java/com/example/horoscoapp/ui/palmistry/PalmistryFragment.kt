bd3ffe161f581c7b312c32b4758f2d12ad2a4668
Enmanuel <manupm747@gmail.com>
2024-04-10 13:47:24 +0200

    PalmistryFragment end 10-04-2024

--- app/src/main/java/com/example/horoscoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horoscoapp/ui/palmistry/PalmistryFragment.kt
@@ -1,20 +1,28 @@
 package com.example.horoscoapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
-import androidx.fragment.app.Fragment
 import com.example.horoscoapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.Exception
+
+//import com.example.horoscoapp.databinding.FragmentPalmistryBinding
 
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
-    companion object{
+    companion object {
         private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
     }
 
@@ -23,37 +31,67 @@ class PalmistryFragment : Fragment() {
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
-    ){isGranted ->
-        if(isGranted){
-            //startCamera
-        }else{
-            Toast.makeText(requireContext(),
-                "Acepta los permisos para poder acceder a la cámara.",
-                Toast.LENGTH_LONG).show()
+    ) { isGranted ->
+        if (isGranted) {
+            startCamera()
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Acepta los permisos para poder disfrutar de una experiencia mágica",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (checkCameraPermission()){
 
-        }else{
+        if (checkCameraPermission()) {
+            startCamera()
+        } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
     }
 
-    private fun checkCameraPermission(): Boolean{
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try{
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }catch (e:Exception){
+                Log.e("aris", "Algo petó ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun checkCameraPermission(): Boolean {
         return PermissionChecker.checkSelfPermission(
-            requireContext(),
-            CAMERA_PERMISSION) ==
-                PermissionChecker.PERMISSION_GRANTED
+            requireContext(), CAMERA_PERMISSION
+        ) == PermissionChecker.PERMISSION_GRANTED
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
